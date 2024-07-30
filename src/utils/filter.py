c990ad89f7dbad45b198eb0ea5485eac8331c28e
Henrique Yuiti Kado <henriqueyuitikado@gmail.com>
2024-07-29 21:05:10 -0300

    Utility classes
    Data: Class for various types of data, such as position and velocity
    Filter: Abstract class for future filter implementations
    State: Class for states

---
+++ src/utils/filter.py
@@ -0,0 +1,9 @@
+from data import Data
+
+class Filter:
+    def __init__(self, pos = Data(0,0), vel = Data(0,0), acc = Data(0,0)) -> None:
+        pass
+    def config(self) -> None:
+        pass
+    def update(self) -> None:
+        pass
