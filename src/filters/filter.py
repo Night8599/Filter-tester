69de2de2416bc53ae78031daded10f9aec512d04
Henrique Yuiti Kado <henriqueyuitikado@gmail.com>
2026-09-01 01:36:32 +0200

    filter

--- src/filters/filter.py
+++ src/filters/filter.py
@@ -8,6 +8,11 @@ class Filter:
     def __init__(self, name=str, config=None):
         self.name = name
         self.config = config
-    def iterate(self, data=None) -> FilterData:
+        self.state_estimate = None
+        self.check_config(config)
+    def iterate(self, data=None) -> None:
         raise NotImplementedError("Subclasses must implement this method.")
+    def check_config(self):
+        # Implement configuration validation logic here
+        pass
     
